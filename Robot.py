cb99fbc6046d81d88c541b67f64b9df5f0397ecf
josephxlam <lamcinel@yahoo.fr>
2016-11-29 16:34:42 +0100

    update robot with moving and looking

--- Robot.py
+++ Robot.py
@@ -3,18 +3,53 @@ import math
 import numpy as np
 
 
-class Robot():
+class Robot:
     def __init__(self, xMax, yMax):
         self.xMax = xMax
         self.yMax = yMax
         self.position = GeneratePoints(1, xMax, yMax)
         self.direction = 2 * math.pi * np.random.rand()
+        self.positionThought = np.array([0,0])
+        self.directionThought = 0.
+        self.detectedPoints = np.array([])
 
-    def walk(self, direction, distance):
+    def walk(self, turnDirection, distance):
         position = self.position
-        self.direction = direction
+        direction = self.direction
+        directionThought = self.directionThought
+        positionThought = self.positionThought
 
-        position[0] = position[0] + distance * math.cos(direction)
-        position[1] = position[1] + distance * math.sin(direction)
+        directionThought = (directionThought + turnDirection) % (2 * math.pi)
+        positionThought += distance * np.array([math.cos(directionThought), math.sin(directionThought)])
+
+        #TODO: Add error parameter in control
+        errorDistance = np.random.normal()
+        errorDirection = np.random.normal()
+
+        distance = max(0, distance + errorDistance)
+        turnDirection += errorDirection
+
+        direction = (direction + turnDirection) % (2 * math.pi)
+        position += distance * np.array([math.cos(direction), math.sin(direction)])
 
         self.position = position
+        self.direction = direction
+        self.directionThought = directionThought
+        self.positionThought = positionThought
+
+    def look(self, pointsRelativePositions):
+        positionThought = self.positionThought
+        directionThought = self.directionThought
+
+        pointsRelativeDistance = pointsRelativePositions[:, 0]
+        pointsRelativeAngle = pointsRelativePositions[:, 1]
+
+        # TODO: Add error parameter in detection
+        pointsRelativeDistance += np.random.normal(size=pointsRelativePositions.shape[0])
+        pointsRelativeAngle += np.random.normal(size=pointsRelativePositions.shape[0])
+
+        pointsAngle = directionThought + pointsRelativeAngle
+
+        detectedPoints = positionThought.reshape(1,2) + pointsRelativeDistance * np.array([math.cos(pointsAngle), math.sin(pointsAngle)])
+
+        self.detectedPoints = detectedPoints
