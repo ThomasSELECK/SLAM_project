98ef9aa0f9a68e68512ea671e82d3fb1457a869b
Evann <evann.courdier@mines-paristech.fr>
2016-11-29 15:28:49 +0100

    add space

--- GeneratePoints.py
+++ GeneratePoints.py
@@ -1,7 +1,8 @@
 import numpy as np
 
-def GeneratePoints(nbPoints,xMax,yMax):
-    points = (np.random.rand(nbPoints,2) * 2 - 1)
-    points[:,0] = xMax * points[:,0]
-    points[:,1] = yMax * points[:,1]
+
+def GeneratePoints(nbPoints, xMax, yMax):
+    points = (np.random.rand(nbPoints, 2) * 2 - 1)
+    points[:, 0] = xMax * points[:, 0]
+    points[:, 1] = yMax * points[:, 1]
     return(points)
