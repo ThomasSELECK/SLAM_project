783451bb9268706d827f2bd039f9337e6ff49ab0
josephxlam <lamcinel@yahoo.fr>
2016-11-29 15:31:24 +0100

    add direction to robot

--- Robot.py
+++ Robot.py
@@ -1,9 +1,11 @@
 import GeneratePoints
+import math
+import numpy as np
 
 class Robot():
     def __init__(self, xMax, yMax):
         self.xMax = xMax
         self.yMax = yMax
         self.Position = GeneratePoints(1,xMax,yMax)
-        self.direction =
+        self.direction = 2 * math.pi * np.random.rand()
         
