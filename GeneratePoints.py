f968cbfa767bbb41b0f96a7362cd39679eab4385
josephxlam <lamcinel@yahoo.fr>
2016-11-29 15:25:39 +0100

    First commit with robot

---
+++ GeneratePoints.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+def GeneratePoints(nbPoints,xMax,yMax):
+    points = (np.random.rand(nbPoints,2) * 2 - 1)
+    points[:,0] = xMax * points[:,0]
+    points[:,1] = yMax * points[:,1]
+    return(points)
---
+++ Robot.py
@@ -0,0 +1,9 @@
+import GeneratePoints
+
+class Robot():
+    def __init__(self, xMax, yMax):
+        self.xMax = xMax
+        self.yMax = yMax
+        self.Position = GeneratePoints(1,xMax,yMax)
+        self.direction =
+        
